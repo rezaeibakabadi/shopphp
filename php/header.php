cf381480bfeca08880eb97bfd72e3c9fe3135167
Reza Eibakabadi <reza.eibak@gmail.com>
2020-08-04 22:34:58 +0430

    Add all my files

---
+++ php/header.php
@@ -0,0 +1,146 @@
+<link href='https://cdn.fontcdn.ir/Font/Persian/Lotus/Lotus.css' rel='stylesheet' type='text/css'>
+
+<link href="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js">
+<link href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js">
+
+<header('');>
+<header id="header" style="font-family: 'Lotus', sans-serif;margin: auto;" >
+
+    <nav class="navbar navbar-dark" style="background-color:#be91009c;">
+
+        <div class="cloud x4" style="margin-left: 5px;">
+            <a href="index.php" class="" style="text-decoration: none;">
+                <h3 class="px-5" style="color: black; border-radius: 50%;filter: url(#filter); box-shadow: 10px 10px 50px 0  #fcffb7;">
+
+                    <i class="fa-shopping"></i>
+
+                    <svg width="30" height="30" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
+                         enable-background="new 0 0 512 512">
+                        <g>
+                            <g>
+                                <path d="m341.5,417.6c-22.9,0-40.7,18.8-40.7,41.7s18.8,41.7 40.7,41.7 40.7-18.8 40.7-41.7-18.8-41.7-40.7-41.7zm0,62.5c-11.5,0-19.8-9.4-19.8-20.9s9.4-20.9 19.8-20.9 19.8,9.4 19.8,20.9-9.4,20.9-19.8,20.9z"/>
+                                <path d="m119.4,417.6c-22.9,0-40.7,18.8-40.7,41.7s18.8,41.7 40.7,41.7c22.9,0 40.7-18.8 40.7-41.7s-18.8-41.7-40.7-41.7zm0,62.5c-11.5,0-19.8-9.4-19.8-20.9s9.4-20.9 19.8-20.9c11.5,0 19.8,9.4 19.8,20.9-2.84217e-14,11.6-9.3,20.9-19.8,20.9z"/>
+                                <path d="m439.5,11l-16.2,63.6h-409.2l75.5,223.7c-25.3,3.7-44.2,25.1-44.2,52.6 0,30.2 22.9,54.2 52.1,54.2h283.6v-19.8h-283.6c-17.7,0-31.3-14.6-31.3-33.4 0-18.8 14.6-33.4 31.3-33.4h283.6 1l74-286.7h41.7v-20.8h-58.3zm-73,286.7h-255.4l-67.8-202.3h374.3l-51.1,202.3z"/>
+                                <rect width="20.9" x="146.5" y="144.4" height="117.8"/>
+                                <rect width="20.9" x="220.6" y="144.4" height="117.8"/>
+                                <rect width="20.9" x="294.6" y="144.4" height="117.8"/>
+                            </g>
+                        </g>
+                    </svg>
+                    <span style="font-family: 'Lotus', sans-serif;font-weight: normal;font-size: 20px;">
+                خرید
+                </span>
+                </h3>
+
+            </a>
+        </div>
+
+
+        <input type="search"  placeholder="جستجو... "
+               style="text-align: right;width: 50%; height: 45px;border: 0; border-radius: 8px; outline: 0; direction: rtl; padding: 8px;letter-spacing: normal;
+    word-spacing: normal;">
+
+
+        <div class="login" style="margin-right: 0;padding-right: 0;">
+
+            <button style="width: 164px;height: 46px;border: 0;border-radius: 5px;outline: 0; background-color:#fff;">
+                <a href="login.php" style="text-align: center;text-decoration: none;color: #616161;">ورود به حساب کاربری
+                <!-- Generator: Adobe Illustrator 19.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+                <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="20" height="20"
+                     viewBox="0 0 53 53" style="enable-background:new 0 0 53 53;" xml:space="preserve">
+<path style="fill:#E7ECED;" d="M18.613,41.552l-7.907,4.313c-0.464,0.253-0.881,0.564-1.269,0.903C14.047,50.655,19.998,53,26.5,53
+	c6.454,0,12.367-2.31,16.964-6.144c-0.424-0.358-0.884-0.68-1.394-0.934l-8.467-4.233c-1.094-0.547-1.785-1.665-1.785-2.888v-3.322
+	c0.238-0.271,0.51-0.619,0.801-1.03c1.154-1.63,2.027-3.423,2.632-5.304c1.086-0.335,1.886-1.338,1.886-2.53v-3.546
+	c0-0.78-0.347-1.477-0.886-1.965v-5.126c0,0,1.053-7.977-9.75-7.977s-9.75,7.977-9.75,7.977v5.126
+	c-0.54,0.488-0.886,1.185-0.886,1.965v3.546c0,0.934,0.491,1.756,1.226,2.231c0.886,3.857,3.206,6.633,3.206,6.633v3.24
+	C20.296,39.899,19.65,40.986,18.613,41.552z"/>
+                    <g>
+                        <path style="fill:#556080;" d="M26.953,0.004C12.32-0.246,0.254,11.414,0.004,26.047C-0.138,34.344,3.56,41.801,9.448,46.76
+		c0.385-0.336,0.798-0.644,1.257-0.894l7.907-4.313c1.037-0.566,1.683-1.653,1.683-2.835v-3.24c0,0-2.321-2.776-3.206-6.633
+		c-0.734-0.475-1.226-1.296-1.226-2.231v-3.546c0-0.78,0.347-1.477,0.886-1.965v-5.126c0,0-1.053-7.977,9.75-7.977
+		s9.75,7.977,9.75,7.977v5.126c0.54,0.488,0.886,1.185,0.886,1.965v3.546c0,1.192-0.8,2.195-1.886,2.53
+		c-0.605,1.881-1.478,3.674-2.632,5.304c-0.291,0.411-0.563,0.759-0.801,1.03V38.8c0,1.223,0.691,2.342,1.785,2.888l8.467,4.233
+		c0.508,0.254,0.967,0.575,1.39,0.932c5.71-4.762,9.399-11.882,9.536-19.9C53.246,12.32,41.587,0.254,26.953,0.004z"/>
+                    </g>
+                    <g>
+                    </g>
+                    <g>
+                    </g>
+                    <g>
+                    </g>
+                    <g>
+                    </g>
+                    <g>
+                    </g>
+                    <g>
+                    </g>
+                    <g>
+                    </g>
+                    <g>
+                    </g>
+                    <g>
+                    </g>
+                    <g>
+                    </g>
+                    <g>
+                    </g>
+                    <g>
+                    </g>
+                    <g>
+                    </g>
+                    <g>
+                    </g>
+                    <g>
+                    </g>
+</svg>
+                </a>
+
+
+
+            </button>
+
+        </div>
+
+
+
+        <div id="navbarNavAltMarkup" style="text-align: right;direction: rtl;">
+
+            <div class="mr-auto">
+
+                <div class="navbar-nav">
+                    <a style="text-decoration: none;display: inline-block;" href="cart.php" class="nav-item nav-link active" >
+
+                        <h5 class="px-5 cart">
+
+                            <div style=" margin-right: 0px;">
+
+                                <i class="fas fa-shopping-cart" style="direction: rtl;color: black; "><span style="font-family: 'Lotus', sans-serif;font-weight: normal;">سبد خرید</span></i>
+
+
+                                <?php
+
+                                if (isset($_SESSION['cart'])) {
+
+                                    $count = count ($_SESSION['cart']);
+                                    echo "<span id=\"cart_count\" class=\"text-warning bg-light\" style=\"text-align: center; padding: 0 0.9rem 0.1rem 0.9rem; border-radius:13px ;\">$count</span>";
+
+                                }
+
+                                else {
+                                    echo "<span id=\"cart_count\" class=\"text-warning bg-light\" style=\"text-align: center; padding: 0 0.9rem 0.1rem 0.9rem; border-radius:3rem ;\">0</span>";
+
+                                }
+
+                                ?>
+                            </div>
+                        </h5>
+
+                    </a>
+                </div>
+            </div>
+        </div>
+
+
+    </nav>
+
+</header>
